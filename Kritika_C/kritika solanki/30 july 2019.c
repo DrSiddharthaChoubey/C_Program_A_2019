d99bfac934789191207957ff80e7555e5c0e0832
KritikaSolanki <solankikritika2001@gmail.com>
2019-07-30 07:49:12 +0530

    Kritika

---
+++ Kritika_C/kritika solanki/30 july 2019.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1,num2;
+    float result;
+    printf("Enter the num1: ");
+    scanf("%d",&num1);
+    printf("Enter the num2: ");
+    scanf("%d",&num2);
+    result = num1+num2;
+    printf("Product = %f",result);
+
+    return 0;
+}
